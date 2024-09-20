1219505dea8c9cead8aa9b01cd9af289d6b59d71
smohi <smohi.wd@gmail.com>
2024-09-20 19:30:48 +0900

    testing py

---
+++ py_basics.py
@@ -0,0 +1,23 @@
+#assigning values to variables
+car_color = 'Red'
+sales = 1500
+price = 2500000.50
+is_popular = True
+
+print(car_color)
+print(sales)
+print(price)
+print(is_popular)
+
+#lists and dictionaries
+#list
+colors = ['Red', 'White', 'Blue', 'Green', 'Black']
+print(colors[1])
+
+#dictionary : key-value pair
+car_info = {
+    "color" : "red",
+    "sales" : 1500,
+    "price" : 2500000.50
+}
+print(car_info['color'])
